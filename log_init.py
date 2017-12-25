961f1680b99fd57844d0418c40df75951e2a40dc
yukito <yk.ito@ntt.com>
2017-12-25 16:22:08 +0900

    first commit

---
+++ log_init.py
@@ -0,0 +1,14 @@
+import logging
+
+LOG_LEVEL = logging.debug
+
+
+def log_init(namespace = ""):
+   logger = logging.getLogger(namespace)
+   logger.setLevel(LOG_LEVEL)
+   handler = logging.FileHandler(LOG_FILE)
+   formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s %(message)s')
+   handler.setFormatter(formatter)
+   handler.setLevel(LOG_LEVEL)
+   logger.addHandler(handler)
+   return logger
