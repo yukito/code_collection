8514b0a692ad44c459fb54db96f071520a6b13fa
yukito <yk.ito@ntt.com>
2018-01-22 14:14:39 +0900

    modified log_init

--- log_init.py
+++ log_init.py
@@ -3,13 +3,24 @@ import logging
 LOG_LEVEL = logging.DEBUG
 LOG_FILE = "/tmp/sample.log"
 
+loggers = {}
 
-def log_init(namespace = ""):
-   logger = logging.getLogger(namespace)
-   logger.setLevel(LOG_LEVEL)
-   handler = logging.FileHandler(LOG_FILE)
-   formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s %(message)s')
-   handler.setFormatter(formatter)
-   handler.setLevel(LOG_LEVEL)
-   logger.addHandler(handler)
-   return logger
+
+def log_init(namespace = __name__):
+
+    if namespace in loggers:
+        return loggers[namespace]
+
+    logger = logging.getLogger(namespace)
+    logger.setLevel(LOG_LEVEL)
+    handler = logging.FileHandler(LOG_FILE)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s:%(name)s %(message)s')
+    handler.setFormatter(formatter)
+    handler.setLevel(LOG_LEVEL)
+    logger.addHandler(handler)
+    logger.propagate = True
+
+    loggers[namespace] = logger
+    logger.debug(namespace + " has been initialized")
+
+    return logger
